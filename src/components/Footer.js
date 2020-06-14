c1d8ae6cab97b7b52b59bae9ed7a6e1b2ba78f56
Ken Sparks <kensparks.dev@gmail.com>
2020-06-14 16:36:06 -0400

    bkg update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ class Footer extends Component {
   render() {
     return (
         <div className="social-icons">
-        <span><i className="fa fa-facebook-f fa-lg-lg"></i></span>
-        <span><i className="fa fa-twitter fa-lg-lg"></i></span>
-          <span><i className="fa fa-instagram fa-lg-lg"></i></span>
+        <span><i className="fa fa-facebook-f fa-lg"></i></span>
+        <span><i className="fa fa-twitter fa-lg"></i></span>
+          <span><i className="fa fa-instagram fa-lg"></i></span>
         </div>
     )
   }
