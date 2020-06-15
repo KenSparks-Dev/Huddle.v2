50b09f5e2f1699d7d687ea2799dce6fae70dc9d9
Ken Sparks <56858524+KenSparks-Dev@users.noreply.github.com>
2020-06-15 13:05:51 -0400

    Animations update.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Footer extends Component {
   render() {
     return (
-        <div className="social-icons">
+        <div className="social-icons fadeIn-icons">
         <span><i className="fa fa-facebook-f fa-lg"></i></span>
         <span><i className="fa fa-twitter fa-lg"></i></span>
           <span><i className="fa fa-instagram fa-lg"></i></span>
