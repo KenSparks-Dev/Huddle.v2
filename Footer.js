49321d892cbfd5c9929f527f8c7d211ec9714460
Ken Sparks <56858524+KenSparks-Dev@users.noreply.github.com>
2020-06-14 21:23:32 -0400

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+  render() {
+    return (
+        <div className="social-icons fadeIn-icons">
+        <span><i className="fa fa-facebook-f fa-lg"></i></span>
+        <span><i className="fa fa-twitter fa-lg"></i></span>
+          <span><i className="fa fa-instagram fa-lg"></i></span>
+        </div>
+    )
+  }
+}
+
+export default Footer
---
+++ Text.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export class Text extends Component {
+	render() {
+		return (
+			<div className="text">
+				<h4 className="fadeIn-midheader">Build The Community Your Fans Will Love</h4>
+				<p className="fadeIn-para">
+					Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create
+					connections with your users as you engage in genuine discussion.
+				</p>
+				<button className="fadeIn-button">Register</button>
+			</div>
+		);
+	}
+}
+
+export default Text;
