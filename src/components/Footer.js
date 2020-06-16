aacd371e622642c4b1b88ad8d2ffbe2f08f6d3d5
Ken Sparks <kensparks.dev@gmail.com>
2020-06-16 09:05:58 -0400

    foolishness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ class Footer extends Component {
   render() {
     return (
         <div className="social-icons fadeIn-icons">
-        <a href="https://www.linkedin.com/in/kensparks90/" target="_blank" ><i className="fa fa-linkedin fa-lg"></i></a>
-        <a href="https://twitter.com/KenSparks90" target="_blank"><i className="fa fa-twitter fa-lg"></i></a>
-        <a href="https://www.instagram.com/kensparks90/" target="_blank"><i className="fa fa-instagram fa-lg"></i></a>
-        <a href="https://github.com/KenSparksDev/Huddle" target="_blank"><i className="fa fa-github fa-lg"></i></a>
+        <a href="https://www.linkedin.com/in/kensparks90/" target="_blank" rel="noopener noreferrer" ><i className="fa fa-linkedin fa-lg"></i></a>
+        <a href="https://twitter.com/KenSparks90" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter fa-lg"></i></a>
+        <a href="https://www.instagram.com/kensparks90/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram fa-lg"></i></a>
+        <a href="https://github.com/KenSparksDev/Huddle" target="_blank" rel="noopener noreferrer"><i className="fa fa-github fa-lg"></i></a>
         </div>
     )
   }
