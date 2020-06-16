ab9f00fa58e1e722c22422c8e541392777b63cd4
Ken Sparks <kensparks.dev@gmail.com>
2020-06-16 09:42:16 -0400

    github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
         <a href="https://www.linkedin.com/in/kensparks90/" target="_blank" rel="noopener noreferrer" ><i className="fa fa-linkedin fa-lg"></i></a>
         <a href="https://twitter.com/KenSparks90" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter fa-lg"></i></a>
         <a href="https://www.instagram.com/kensparks90/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram fa-lg"></i></a>
-        <a href="https://github.com/KenSparksDev/Huddle" target="_blank" rel="noopener noreferrer"><i className="fa fa-github fa-lg"></i></a>
+        <a href="https://github.com/KenSparksDev/Huddle.v2" target="_blank" rel="noopener noreferrer"><i className="fa fa-github fa-lg"></i></a>
         </div>
     )
   }
