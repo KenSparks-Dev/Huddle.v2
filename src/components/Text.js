874a77b171dd208b93ce6526519f7ee5d47f9be3
Ken Sparks <56858524+KenSparks-Dev@users.noreply.github.com>
2020-06-16 02:02:08 -0400

    Animations Update

--- src/components/Text.js
+++ src/components/Text.js
@@ -4,12 +4,12 @@ export class Text extends Component {
 	render() {
 		return (
 			<div className="text">
-				<h4 className="fadeIn-midheader">Build The Community Your Fans Will Love</h4>
-				<p className="fadeIn-para">
+				<h4 className="fadeIn-text">Build The Community Your Fans Will Love</h4>
+				<p className="fadeIn-text">
 					Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create
 					connections with your users as you engage in genuine discussion.
 				</p>
-				<button className="fadeIn-button">Register</button>
+				<button className="fadeIn-text">Register</button>
 			</div>
 		);
 	}
